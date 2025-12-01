494dcd774585a3a9a87fa36c3ef2d307d20a74e2
ornel77 <ornella.tomboza@gmail.com>
2025-12-01 18:33:36 +0100

    implement window to read text

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,6 +2,7 @@ import Terminal from "./Terminal";
 import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
+import Text from "./Text";
 
 
-export {Terminal, Safari, Resume, Finder}
+export {Terminal, Safari, Resume, Finder, Text}
